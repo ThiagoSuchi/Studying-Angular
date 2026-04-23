f331167a0553b68c8948863c3f6328cc69b640e8
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-04-23 17:17:55 -0400

    feat(counter): adiciona logica de incremento, decremento e reset com signals

--- src/app/screens/counter/counter.ts
+++ src/app/screens/counter/counter.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Title } from "../../components/shared/title/title";
 
 @Component({
   selector: 'app-counter',
-  imports: [],
+  imports: [Title],
   templateUrl: './counter.html',
   styleUrl: './counter.css',
 })
-export class Counter {}
+export class Counter {
+  count = signal(0);
+
+  increment() {
+    this.count.update(value => value + 1);
+  }
+
+  decrement() {
+    if (this.count() != 0)
+    this.count.update(value => value - 1);
+
+    return
+  }
+
+  reset() {
+    this.count.set(0);
+  }
+}
