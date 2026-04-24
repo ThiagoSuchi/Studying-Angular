16b1cfbff2e6b38f7861e6bb7cafc73bf96e925c
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-04-24 17:54:16 -0400

    feat(todos): implement remove task logic

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -11,12 +11,22 @@ export class TodosService {
     { id: crypto.randomUUID(), title: "Comprar parafusos para madeira", completed: false },
     { id: crypto.randomUUID(), title: "Medir área da varanda", completed: true },
     { id: crypto.randomUUID(), title: "Comprar óculos de proteção", completed: false },
-    { id: crypto.randomUUID(), title: "Cortar ripas de pinus", completed: false },
+    { id: crypto.randomUUID(), title: "Cortar aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaripas de pinus", completed: false },
   ]);
 
   readonly items = this._items.asReadonly();
 
   itemCompleted(item: Todo) {
     item.completed = !item.completed;
+  }
+
+  delete = signal(false);
+
+  showDelete() {
+    this.delete.update(val => !val);
+  }
+
+  removeTask(id: string) {
+    this._items.update(item => item.filter(item => item.id != id));
   }
 }
